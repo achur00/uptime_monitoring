eb278f8307b582267f9bd850f879caaafe305595
achur00 <yachur@ymail.com>
2026-05-17 19:39:36 +0100

    feat(relationship + mass assigning) add mass assignments in monitor and monitor event, and allow relationship between them

--- app/Models/Monitor.php
+++ app/Models/Monitor.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Monitor extends Model
 {
-    //
+  protected $table = 'monitors';
+  
+   protected $fillable = [
+        'url',
+        'check_interval',
+        'threshold',
+        'status',
+    ];
+
+    public function checks()
+    {
+        return $this->hasMany(
+            MonitorEvent::class
+        );
+    }
 }
--- app/Models/MonitorEvent.php
+++ app/Models/MonitorEvent.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class MonitorEvent extends Model
 {
     protected $table = 'monitor_events';
+
+        protected $fillable = [
+        'monitor_id',
+        'status_code',
+        'response_time_ms',
+        'is_up',
+        'checked_at'
+    ];
+
+    public function monitor()
+    {
+        return $this->belongsTo(Monitor::class);
+    }
 }
